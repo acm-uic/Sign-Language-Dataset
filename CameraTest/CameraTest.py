e350a71e373a32cb633eed87afcc332ddf00d802
sammypatel06 <sammypatel06@gmail.com>
2024-11-16 00:00:15 -0600

    Update CameraTest.py

--- CameraTest/CameraTest.py
+++ CameraTest/CameraTest.py
@@ -1,33 +1,68 @@
 import cv2
+import mediapipe as mp
+
+# Initalize MediaPipe Hands and drawing utilities
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
 
 # Open the default camera
 cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    print("Camera inaccessible.")
+    exit()
 
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# Setting up the MediaPipe Hands object
+with mp_hands.Hands(
+        model_complexity = 1, 
+        min_detection_confidence = 0.5, 
+        min_tracking_confidence = 0.5
+    ) as hands:
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+    
+    while cam.isOpened():                   # Loop runs continuously as long as the video capture object is open
+        captured, frame_bgr = cam.read()    # Attempts to capture next frame
+        if not captured:                    # Failed to capture next frame
+            print("Camera inaccessible.")
+            break
 
-while True:
-    ret, frame = cam.read()
 
-    # Flip image
-    flip_horizontal = cv2.flip(frame, 1)
+        # MediaPipe expects the input image to be in the RGB color format
+        # OpenCV displays video frames in BGR
+        # A copy of the frame in each format is made
 
-    # Write the frame to the output file
-    out.write(flip_horizontal)
+        frame_bgr = cv2.flip(frame_bgr, 1) # Flips frame
+        frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
 
-    # Display the captured frame
-    cv2.imshow('Camera', flip_horizontal)
+        res = hands.process(frame_rgb)  # The RGB format is used for processing
 
-    # Press 'q' to exit the loop
-    if cv2.waitKey(1) == ord('q'):
-        break
+        # Draw hand landmarks on feed
+        if res.multi_hand_landmarks:                        # Check if hand landmarks have been found
+            for hand_landmarks in res.multi_hand_landmarks: # Iterate over each hand landmark found
+                mp_drawing.draw_landmarks(                  # Draws landmarks and connections
+                    frame_bgr, hand_landmarks, 
+                    mp_hands.HAND_CONNECTIONS,
 
-# Release the capture and writer objects
+                    # Draws landmarks
+                    mp_drawing.DrawingSpec(                 
+                        color = (0, 255, 0), 
+                        thickness = 2, 
+                        circle_radius = 2
+                    ), 
+
+                    # Draws connections between landmarks
+                    mp_drawing.DrawingSpec(                 
+                        color = (255, 0, 0), 
+                        thickness = 2
+                    )
+                )
+        
+        cv2.imshow("Hand Detection Test", frame_bgr)    # The BGR format is used for displaying the frame
+
+        # Program exit (Press 'q')
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# Release webcame and close window
 cam.release()
-out.release()
 cv2.destroyAllWindows()
